64ef6c577ab8a83e32262c51f977f424436d384c
Riceball LEE <snowyu.lee@gmail.com>
2022-09-27 15:04:48 +0800

    fix(tauri): win32 can not detect system language

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,18 @@
     windows_subsystem = "windows"
 )]
 
+use sys_locale::get_locale;
+
+// const invoke = window.__TAURI__.invoke
+// const currentLang = await invoke("lang")
+#[tauri::command]
+fn lang() -> String {
+    // format!("Hello, {}! You've been greeted from Rust!", name)
+    let locale = get_locale().unwrap_or_else(|| String::from("en-US"));
+    locale.into()
+}
+
+// const hi = await invoke("greet", {name: "Mike"})
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +23,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, lang])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
